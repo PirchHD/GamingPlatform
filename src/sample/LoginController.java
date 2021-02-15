6e22369a7040aaa6af81e2237f4a80b15b049b46
Prochal Szymon <34886043+PirchHD@users.noreply.github.com>
2021-02-15 20:25:28 +0100

    working cancel button + modern UI

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,15 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
-
 public class LoginController {
+
     @FXML
     private Button cancelButton;
 
-    public void cancelButtonOnAction(javafx.event.ActionEvent actionEvent) {
+    @FXML
+    private void cancelButtonOnAction(ActionEvent actionEvent) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("Hello World");
+
         primaryStage.setScene(new Scene(root, 564, 428));
         primaryStage.show();
     }
