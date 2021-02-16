edfaffec3bb2918e1aaa6447ca7bf1273a134be2
Prochal Szymon <34886043+PirchHD@users.noreply.github.com>
2021-02-16 16:29:58 +0100

    add loginButton + invalisMessage test

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -2,18 +2,35 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
 
     @FXML
     private Button cancelButton;
+    @FXML
+    private Label invalidMessage;
+
+    public void loginButtonOnAction(){
+        System.out.println("works");
+        invalidMessage.setText("works");
+    }
 
     @FXML
     private void cancelButtonOnAction(ActionEvent actionEvent) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 
 }
